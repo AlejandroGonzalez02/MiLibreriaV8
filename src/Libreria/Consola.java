dcb8b756c43196389f38a6fdf6de3201af215adc
AlejandroGonzalez02 <agonzalezalonso@danielcastelao.org>
2021-05-13 18:25:44 +0200

    Initial commit

---
+++ src/Libreria/Consola.java
@@ -0,0 +1,7 @@
+package Libreria;
+
+public class Consola {
+    public void mensaxe(){
+        System.out.println("Mensaxe por consola.");
+    }
+}
---
+++ src/Libreria/Ventana.java
@@ -0,0 +1,9 @@
+package Libreria;
+
+import javax.swing.*;
+
+public class Ventana {
+    public void mensaxe(){
+        JOptionPane.showInputDialog("Mensaxe por ventana.");
+    }
+}
